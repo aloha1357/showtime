7b811e921cb0df33d282c83ad98de55e2f53b33a
aloha1357 <64310247+aloha1357@users.noreply.github.com>
2024-04-02 23:27:19 +0800

    Update Angular dependencies and styling ,Search Bar....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component,HostListener } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'showtime';
+  showSearch = false; // 控制搜索框显示的属性
+  searchTerm: string = ''; // Add searchTerm property
+
+  toggleSearch(event?: MouseEvent): void {
+    // 如果已经显示搜索输入框，那么点击按钮应该是提交搜索
+    if (this.showSearch) {
+      this.onSearch();
+    } else {
+      this.showSearch = true;
+      event?.preventDefault(); // 阻止表单提交
+    }
+  }
+
+  onSearch(): void {
+    // 执行搜索操作，比如调用服务或者跳转页面
+    console.log('Searching for: ', this.searchTerm);
+    this.showSearch = false; // 搜索后隐藏输入框
+  }
 }
