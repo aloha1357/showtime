b7b51fd6d339f26592098d168cd173d4011dff5a
aloha1357 <64310247+aloha1357@users.noreply.github.com>
2024-04-04 22:04:43 +0800

    Refactor movie details component and search component

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieApiServiceService } from '../../service/movie-api-service.service'; // Update the import path
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-movie-details',
@@ -10,15 +11,17 @@ import { MovieApiServiceService } from '../../service/movie-api-service.service'
   templateUrl: './movie-details.component.html',
   styleUrl: './movie-details.component.css'
 })
+
 export class MovieDetailsComponent {
 
   MovieDetailResult: any=[];
   MovieVideoResult: any=[];
   MovieReviewResult: any=[];
   MovieCastResult: any=[];
-  constructor(private movieApiService: MovieApiServiceService, private activatedRoute: ActivatedRoute) { } // Declare activatedRoute as a private property
-
-  // ...
+  constructor(private movieApiService: MovieApiServiceService,
+              private activatedRoute: ActivatedRoute,
+              private sanitizer: DomSanitizer
+              ) { } // Declare activatedRoute as a private property
 
   ngOnInit(): void {
     let getParamId = this.activatedRoute.snapshot.paramMap.get('id'); // Use this.activatedRoute instead of this.router
@@ -29,7 +32,7 @@ export class MovieDetailsComponent {
   }
 
   getMovieDetails(id: any){  
-    this.movieApiService.getMovieDetails(id).subscribe((data: any) => { // Specify the type of 'data' as 'any'
+    this.movieApiService.getMovieDetails(id).subscribe((data: any) => { 
       console.log(data, 'movie details');
       this.MovieDetailResult = data;
     });
@@ -45,15 +48,22 @@ export class MovieDetailsComponent {
     });
   }
   getMovieReviews(id: any){
-    this.movieApiService.getMovieReviews(id).subscribe((data: any) => { // Specify the type of 'data' as 'any'
+    this.movieApiService.getMovieReviews(id).subscribe((data: any) => { 
       console.log(data, 'movie reviews');
       this.MovieReviewResult = data.results;
     });
   }
   getMovieCast(id: any){
-    this.movieApiService.getMovieCast(id).subscribe((data: any) => { // Specify the type of 'data' as 'any'
+    this.movieApiService.getMovieCast(id).subscribe((data: any) => { 
       console.log(data, 'movie cast');
       this.MovieCastResult = data.cast;
     });
+  }
+
+
+  getSafeUrl(videoId: string): SafeResourceUrl {
+    // 注意：为了自动播放并静音，我们添加了`autoplay=1`和`mute=1`
+    let url = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=0`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
